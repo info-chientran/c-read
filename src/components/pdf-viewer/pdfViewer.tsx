0d48a54b349c97d5b5d13ad27bca904faf6215af
Chien Tran <info.chientran@gmail.com>
2024-04-22 17:11:59 +0700

    #1 update base

--- src/components/pdf-viewer/pdfViewer.tsx
+++ src/components/pdf-viewer/pdfViewer.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import { SpecialZoomLevel, Viewer, Worker } from "@react-pdf-viewer/core";
 
-import en_US from "./Languages/en_US.json";
-import vi_VN from "./Languages/vi_VN.json";
-
 import "@react-pdf-viewer/core/lib/styles/index.css";
 
 interface Props {
@@ -13,8 +10,6 @@ interface Props {
 }
 
 const PdfViewer: React.FC<Props> = ({ fileUrl, plugins, className }) => {
-  const [language, setLanguage] = useState(en_US);
-
   return (
     <Worker
       workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}
@@ -24,7 +19,6 @@ const PdfViewer: React.FC<Props> = ({ fileUrl, plugins, className }) => {
           fileUrl={fileUrl}
           plugins={plugins}
           defaultScale={SpecialZoomLevel.PageFit}
-          localization={language}
         />
       </div>
     </Worker>
