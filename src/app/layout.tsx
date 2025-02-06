1ccc33ff2e5a0edee95c88bb05943f26f110fb49
Chien Tran <info.chientran@gmail.com>
2025-02-06 14:04:31 +0700

    Update format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/assets/scss/globals.scss";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/assets/scss/globals.scss';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Pdf viewer",
-  description: "",
+    title: 'Pdf viewer',
+    description: '',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
--- src/components/pdf-viewer/pdfViewer.tsx
+++ src/components/pdf-viewer/pdfViewer.tsx
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
-import { SpecialZoomLevel, Viewer, Worker } from "@react-pdf-viewer/core";
+import React, { useState } from 'react';
+import { SpecialZoomLevel, Viewer, Worker } from '@react-pdf-viewer/core';
 
-import "@react-pdf-viewer/core/lib/styles/index.css";
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 interface Props {
-  fileUrl: string;
-  plugins: any[];
-  className: string;
+    fileUrl: string;
+    plugins: any[];
+    className: string;
 }
 
 const PdfViewer: React.FC<Props> = ({ fileUrl, plugins, className }) => {
-  return (
-    <Worker
-      workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}
-    >
-      <div className={className}>
-        <Viewer
-          fileUrl={fileUrl}
-          plugins={plugins}
-          defaultScale={SpecialZoomLevel.PageFit}
-        />
-      </div>
-    </Worker>
-  );
+    return (
+        <Worker
+            workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}
+        >
+            <div className={className}>
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={plugins}
+                    defaultScale={SpecialZoomLevel.PageFit}
+                />
+            </div>
+        </Worker>
+    );
 };
 
 export default PdfViewer;
