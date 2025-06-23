97f5faca04d3f8a67de1289ce21f049c02016436
Chien Tran <info.chientran@gmail.com>
2025-06-24 05:59:11 +0700

    Fix rule: function name

--- src/app/view/epub/page.tsx
+++ src/app/view/epub/page.tsx
@@ -3,22 +3,22 @@
 import { useState } from "react"
 import { ReactReader } from "react-reader"
 
-export default function epubPage() {
-      const [location, setLocation] = useState<string | number>(0)
+export default function EpubPage() {
+    const [location, setLocation] = useState<string | number>(0);
 
-  return (
-    <div>
-        <div style={{ margin: 'auto', height: '100vh', maxWidth: '768px' }}>
-            <ReactReader
-                url="/mindset.epub"
-                location={location}
-                locationChanged={(epubcfi: string) => setLocation(epubcfi)}
-                epubOptions={{
-                    flow: 'scrolled',
-                    manager: 'continuous',
-                }}
-            />
+    return (
+        <div>
+            <div style={{ margin: 'auto', height: '100vh', maxWidth: '768px' }}>
+                <ReactReader
+                    url="/mindset.epub"
+                    location={location}
+                    locationChanged={(epubcfi: string) => setLocation(epubcfi)}
+                    epubOptions={{
+                        flow: 'scrolled',
+                        manager: 'continuous',
+                    }}
+                />
+            </div>
         </div>
-    </div>
-  )
+    );
 }
