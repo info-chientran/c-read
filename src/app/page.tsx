5e4ffa994af1db854fff7efd5a61d4d50208677e
Chien Tran <info.chientran@gmail.com>
2025-07-10 04:59:19 +0700

    Update next 15, mui and home UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,39 @@
 
 import Link from 'next/link';
 import '@/assets/scss/app/home.scss';
+import { Avatar, Box, Container, Stack } from '@mui/material';
+import Logo from '@/components/icons/Logo';
 
 export default function Home() {
     return (
-        <div className='h-screen flex justify-center items-center bg-[#f6f8fa]'>
-            <div className='max-w-4xl flex gap-x-8 w-full'>
-                <div className='bg-white px-8 py-4 rounded-md w-1/2 shadow-md'>
-                    <Link className='bg-[#00c0b8] text-white py-1 rounded-sm w-full inline-block text-center' href="/view/epub">Epub</Link>
-                </div>
+        <Container>
+            <Stack direction="row"
+                spacing={2} sx={{
+                    marginTop: '24px',
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', columnGap: '16px', padding: '8px 0' }}>
+                    <Logo />
 
-                <div className='bg-white px-8 py-4 rounded-md w-1/2 shadow-md'>
-                    <Link className='bg-[#009cec] text-white py-1 rounded-sm w-full inline-block text-center' href="/view/pdf">Pdf</Link>
+                    <div>
+                        <p className='text-4xl'>
+                            Chooibook
+                        </p>
+                        <p className='text-sm text-center'>Books here, stories there</p>
+                    </div>
+                </Box>
+
+                <div>
+                    <Avatar sx={{ width: 24, height: 24 }}>C</Avatar>
                 </div>
+            </Stack>
+
+            <div className='h-14 bg-[#3A3A3C] flex mt-4'>
+                <Link className='text-white px-6 py-4 text-center' href="/view/epub">Epub</Link>
+
+                <Link className='text-white px-6 py-4 text-center' href="/view/pdf">Pdf</Link>
             </div>
-        </div>
+        </Container>
     );
 }
---
+++ src/components/icons/Logo.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+const Logo = () => {
+    return (
+        <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_1_12979" style={{ maskType: 'alpha' }} maskUnits="userSpaceOnUse" x="0" y="0" width="52" height="52">
+                <rect width="52" height="52" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_1_12979)">
+                <path d="M10.6358 4.01855L9.5249 32.1269C9.48472 33.1173 10.2765 33.9422 11.2669 33.9422L39.3398 33.9989C40.2805 33.9989 41.0534 33.2591 41.0889 32.3207L42.1336 5.89292C42.1738 4.90255 41.382 4.07765 40.3916 4.07765L10.6358 4.01855Z" fill="url(#paint0_linear_1_12979)" />
+                <path d="M10.6359 4.01855L9.4541 33.9398L35.9197 38.4426L37.1016 8.52128L10.6359 4.01855Z" fill="url(#paint1_linear_1_12979)" />
+                <path d="M10.6359 4.01855L9.4541 33.9398L30.8379 43.8056L32.0221 13.8844L10.6359 4.01855Z" fill="url(#paint2_linear_1_12979)" />
+                <path d="M10.6359 4.01855L9.4706 33.4978C9.45878 33.7791 9.56751 34.0533 9.76605 34.2518L22.9646 47.3606C24.0188 48.4076 25.8151 47.7009 25.8719 46.2166L26.8717 20.9233C26.8906 20.4458 26.7086 19.9826 26.3682 19.6446L10.6359 4.01855Z" fill="url(#paint3_linear_1_12979)" />
+            </g>
+            <defs>
+                <linearGradient id="paint0_linear_1_12979" x1="9.56842" y1="17.7477" x2="42.027" y2="20.2738" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#43C5ED" />
+                    <stop offset="0.42" stop-color="#43D3D9" />
+                    <stop offset="1" stop-color="#43EDB8" />
+                </linearGradient>
+                <linearGradient id="paint1_linear_1_12979" x1="9.38165" y1="20.149" x2="37.1743" y2="22.312" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#158CFF" />
+                    <stop offset="0.32" stop-color="#209AFA" />
+                    <stop offset="0.94" stop-color="#3FC0EE" />
+                    <stop offset="1" stop-color="#43C5ED" />
+                </linearGradient>
+                <linearGradient id="paint2_linear_1_12979" x1="9.15883" y1="23.0122" x2="32.3139" y2="24.8142" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#0040F2" />
+                    <stop offset="0.26" stop-color="#0657F5" />
+                    <stop offset="0.75" stop-color="#107DFC" />
+                    <stop offset="1" stop-color="#158CFF" />
+                </linearGradient>
+                <linearGradient id="paint3_linear_1_12979" x1="16.531" y1="47.2724" x2="19.8415" y2="4.73504" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#0040F2" />
+                    <stop offset="0.18" stop-color="#313DF2" />
+                    <stop offset="0.37" stop-color="#5D3BF2" />
+                    <stop offset="0.55" stop-color="#7F39F2" />
+                    <stop offset="0.72" stop-color="#9838F2" />
+                    <stop offset="0.87" stop-color="#A637F2" />
+                    <stop offset="1" stop-color="#AC37F2" />
+                </linearGradient>
+            </defs>
+        </svg>
+    )
+}
+
+export default Logo
